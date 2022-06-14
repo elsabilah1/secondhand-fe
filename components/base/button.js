7fdaa284c9e15f0ed15fe67df523f29d2913d184
raihandimasarifin <72178112+raihandimasarifin@users.noreply.github.com>
2022-06-14 17:16:48 +0700

    button

---
+++ components/base/button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function button() {
+  return (
+    <div className='button'>
+      <button class="bg-primary-04 hover:bg-primary-04 text-white font-bold py-2 px-4 rounded-full">
+        Button
+      </button>
+    </div>
+
+  )
+}
