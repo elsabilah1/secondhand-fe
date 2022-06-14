31dae501d03a3dc3929895fe823f992b5880d85a
hdschae <ikmalhds@gmail.com>
2022-06-14 21:16:10 +0700

    Fix InputField

--- components/base/InputField.js
+++ components/base/InputField.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import Text from './Text'
+import FeatherIcon from 'feather-icons-react';
+import { useState } from 'react';
 
 export default function InputField({ label, type, value, name, placeholder, onChange }) {
+    const [icon, setIcon] = useState(false);
   return (
-    <div>
-        <label htmlFor="input-field" className="block text-gray-700 text-sm font-bold mb-2"><Text>{label}</Text></label>
-        <input 
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type={type}
-            value={value}
-            name={name}
-            placeholder={placeholder}
-            onChange={onChange}
-        />
+    <div className='w-full'>
+        <label htmlFor="input-field" className="block mb-2"><Text type='body/12'>{label}</Text></label>
+        <div className='relative'>
+            <input 
+                className="appearance-none border-neutral-02 border rounded-2xl
+                          w-full py-3 px-4 text-sm bg-neutral-01 text-neutral-05
+                         focus:outline-none focus:shadow-outline 
+                         placeholder:text-neutral-03 placeholder:text-sm"
+                type={type}
+                value={value}
+                name={name}
+                placeholder={placeholder}
+                onChange={onChange}
+            />
+            {type === "password" && 
+            <button onClick={()=> setIcon(!icon)}>
+                <FeatherIcon icon={!icon ? 'eye':'eye-off'} className='absolute right-0 inset-y-0 translate-y-1/2 mr-4 text-neutral-03' size={24} />
+            </button>}
+        </div>
     </div>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,28 @@
 import LayoutAuth from '../components/layout/LayoutAuth'
+import InputField from '../components/base/InputField'
 
 export default function Login() {
   return (
     <LayoutAuth>
-      <div className="">form login</div>
+      <div className='w-full'>
+      Masuk
+      <InputField 
+        type="email"
+        // value=""
+        placeholder="Contoh: johndee@gmail.com"
+        label="Email"
+        name="email"
+        // onChange=""
+      />
+      <InputField 
+        type="password"
+        // value=""
+        placeholder="Masukkan password"
+        label="Password"
+        name="password"
+        // onChange=""
+      />
+      </div>
     </LayoutAuth>
   )
 }
