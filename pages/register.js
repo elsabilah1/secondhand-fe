381e829fa8dc19af1ae0650cdbf2f1dfcfd5374d
raihandimasarifin <72178112+raihandimasarifin@users.noreply.github.com>
2022-06-14 23:04:32 +0700

    Create Register Page

--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,52 @@
+import Text from '../components/base/Text'
 import LayoutAuth from '../components/layout/LayoutAuth'
+import InputField from '../components/base/InputField'
+import Button from '../components/base/Button'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Register() {
+  const router = useRouter();
   return (
     <LayoutAuth>
-      <div>Register</div>
+       <div className='w-full space-y-6'>
+        <Text type="heading/24" weight="bold">Daftar</Text>
+        <div className='space-y-4'>
+          <InputField 
+            type="Nama"
+            // value=""
+            placeholder="Nama Lengkap"
+            label="Nama"
+            name="nama"
+            // onChange=""
+          />
+          <InputField 
+            type="email"
+            // value=""
+            placeholder="Contoh: johndee@gmail.com"
+            label="Email"
+            name="email"
+            // onChange=""
+          />
+          <InputField 
+            type="password"
+            // value=""
+            placeholder="Masukkan password"
+            label="Password"
+            name="password"
+            // onChange=""
+          />
+        </div>
+        <Button width="full" onClick={() => router.push("/login")}>
+          Daftar
+        </Button>
+        <div className='w-full flex gap-1 justify-center'>
+          <Text>Sudah punya akun?</Text>
+          <Link href="/login">
+            <a className='text-primary-04 hover:text-primary-03'><Text weight="bold">Masuk di sini</Text></a>
+          </Link>
+        </div>
+      </div>
     </LayoutAuth>
   )
 }
