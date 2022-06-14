cd5cb28f5b3fe3df23e32ce62eb5e719e7718bd2
hdschae <ikmalhds@gmail.com>
2022-06-14 17:14:50 +0700

    Create Component InputField

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Text from '../components/base/Text'
+import InputField from '../components/base/InputField'
 
 export default function Home() {
   return (
@@ -12,6 +13,14 @@ export default function Home() {
 
       <div className="grid h-screen place-items-center">
         <Text>Text sample</Text>
+          <InputField 
+            type="text"
+            value=""
+            placeholder="login"
+            label="Login"
+            name="login"
+            onChange=""
+          />
       </div>
     </div>
   )
