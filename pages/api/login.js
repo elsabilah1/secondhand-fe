a6964fcfb55f691f6dec829bc8daf3fe4369592e
Elsa Sabilah <elsabilah1@gmail.com>
2022-06-18 00:29:11 +0700

    fix login handling

---
+++ pages/api/login.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Post } from '../../utils/Api'
+
+export default async (req, res) => {
+  const { method, headers, body } = req
+
+  if (method !== 'POST') {
+    res.status(404).end()
+  }
+
+  try {
+    const { data, headers: returnedHeaders } = await Post('/auth/login', body)
+
+    Object.keys(returnedHeaders).forEach((key) =>
+      res.setHeader(key, returnedHeaders[key]),
+    )
+
+    res.status(200).json(data)
+  } catch (e) {
+    const { response } = e
+    const { status, data } = response
+    res.status(status).json(data)
+  }
+}
