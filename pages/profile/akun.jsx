073658726e94ce89b70c07f57166ab1cd459a1c5
raihandimasarifin <72178112+raihandimasarifin@users.noreply.github.com>
2022-06-23 20:40:37 +0700

    akun

--- pages/profile/akun.jsx
+++ pages/profile/akun.jsx
@@ -32,72 +32,71 @@ export default withRouter(function Akun({ router }) {
       </div>
     ))
   return (
-    <div className="mx-auto my-6 max-w-2xl px-4 md:my-10">
-        <div>
-        <Text weight="bold" type='heading/20'>Akun Saya</Text>
-        </div>
+    <div>
+        <div className="mx-auto my-6 max-w-2xl px-4 md:my-10">
+            <div>
+            <Text weight="bold" type='heading/20'>Akun Saya</Text>
+            </div>
 
-        <div className="w-full space-y-4 md:w-10/12">
-          <div className="relative flex justify-center">
-            <Dropzone maxFiles={1} onDrop={onDrop}>
-              <button className="group h-24 w-24 rounded-xl border border-primary-01 bg-primary-01 active:scale-95 group-hover:border-primary-03">
-                <FeatherIcon
-                  icon="camera"
-                  className="inline h-6 w-6 text-primary-04 active:scale-95 group-hover:text-primary-03"
-                />
-              </button>
-            </Dropzone>
-            <div className="absolute">{selected_images}</div>
-          </div>
-        </div>
+            <div className="relative flex justify-center">
+              <Dropzone maxFiles={1} onDrop={onDrop}>
+                <button className="group h-24 w-24 rounded-xl border border-primary-01 bg-primary-01 active:scale-95 group-hover:border-primary-03">
+                  <FeatherIcon
+                    icon="camera"
+                    className="inline h-6 w-6 text-primary-04 active:scale-95 group-hover:text-primary-03"
+                  />
+                </button>
+              </Dropzone>
+              <div className="absolute">{selected_images}</div>
+            </div>
 
-        <div>
-          <div className='gap-4 overflow-x-auto'>
-              <button className='flex gap-4'>
-                  <FeatherIcon className="inline text-primary-04" icon='edit-3' />
-                  <Text weight="medium">Ubah Akun</Text>
-              </button>
-              <div className='border border-b'></div>
+            <div>
+              <div className='gap-4 overflow-x-auto py-4'>
+                  <button className='flex gap-4'>
+                      <FeatherIcon className="inline text-primary-04" icon='edit-3' />
+                      <Text weight="medium">Ubah Akun</Text>
+                  </button>
+                  <div className='border border-b'></div>
+              </div>
+              <div className='gap-4 overflow-x-auto py-4'>
+                  <button className='flex gap-4'>
+                      <FeatherIcon className="inline text-primary-04" icon='settings' />
+                      <Text weight="medium">Pengaturan Akun</Text>
+                  </button>
+                  <div className='border border-b'></div>
+              </div>
+              <div className='gap-4 overflow-x-auto py-4'>
+                  <button className='flex gap-4'>
+                      <FeatherIcon className="inline text-primary-04" icon='log-out' />
+                      <Text weight="medium">Keluar</Text>
+                  </button>
+                  <div className='border border-b'></div>
+              </div>
+              <div className='flex justify-center text-neutral-03'>
+              <Text>Version 1.0</Text>
+              </div>
+            </div>
           </div>
-          <div className='gap-4 overflow-x-auto'>
-              <button className='flex gap-4'>
-                  <FeatherIcon className="inline text-primary-04" icon='settings' />
-                  <Text weight="medium">Pengaturan Akun</Text>
-              </button>
-              <div className='border border-b'></div>
-          </div>
-          <div className='gap-4 overflow-x-auto'>
-              <button className='flex gap-4'>
-                  <FeatherIcon className="inline text-primary-04" icon='log-out' />
-                  <Text weight="medium">Keluar</Text>
-              </button>
-              <div className='border border-b'></div>
-          </div>
-          <div className='flex justify-center text-neutral-03'>
-          <Text>Version 1.0</Text>
-          </div>
-        </div>
-
-        <div className='flex gap-4 justify-center'>
-            <button className='gap-4 hover:text-primary-04'>
+          <div className='flex gap-4 justify-center border'>
+            <button className='gap-4 hover:text-primary-04 text-neutral-03'>
                 <FeatherIcon className='' icon='home' />
-                <Text weight="medium">Home</Text>
+                <Text>Home</Text>
             </button>
-            <button className='gap-4 hover:text-primary-04'>
+            <button className='gap-4 hover:text-primary-04 text-neutral-03'>
                 <FeatherIcon className='' icon='bell' />
-                <Text weight="medium">Notifikasi</Text>
+                <Text>Notifikasi</Text>
             </button>
-            <button className='gap-4 hover:text-primary-04'>
+            <button className='gap-4 hover:text-primary-04 text-neutral-03'>
                 <FeatherIcon className='' icon='plus-circle' />
-                <Text weight="medium">Jual</Text>
+                <Text>Jual</Text>
             </button>
-            <button className='gap-4 hover:text-primary-04'>
+            <button className='gap-4 hover:text-primary-04 text-neutral-03'>
                 <FeatherIcon className='' icon='menu' />
-                <Text weight="medium">Daftar Jual</Text>
+                <Text>Daftar Jual</Text>
             </button>
-            <button className='gap-4 hover:text-primary-04'>
+            <button className='gap-4 hover:text-primary-04 text-neutral-03'>
                 <FeatherIcon className='' icon='user' />
-                <Text weight="medium">Akun</Text>
+                <Text>Akun</Text>
             </button>
         </div>
     </div>
